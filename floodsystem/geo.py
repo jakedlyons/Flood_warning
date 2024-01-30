e64dace9f7baa32fa2081b837a84609fc40d933a
jakedlyons <jakedenislyons@gmail.com>
2024-01-30 14:54:50 +0000

    clear geo.py

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -3,16 +3,6 @@
 # SPDX-License-Identifier: MIT
 """This module contains a collection of functions related to
 geographical data.
-
 """
 
-#from .utils import sorted_by_key  # noqa
-
-def addition(a, b):
-    return a + b
-
-print(addition(5,4))
 from .utils import sorted_by_key  # noqa
-
-def test1(a,b):
-    return a*b
