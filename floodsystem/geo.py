769b854c59b49014aa1ff9b71faf418e0d50195e
Han Gu <hg510@cam.ac.uk>
2024-01-30 16:01:51 +0000

    geo.py edit for Task1C

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,3 +6,17 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+
+def rivers_with_station(stations):
+    rivers = set()
+    for station in stations:
+        rivers.add(station.river)
+    sorted_by_key(rivers,0)
+    rivers = list(rivers)
+    return rivers
+
+def stations_by_river(stations):
+    by_river = dict.fromkeys(rivers_with_station(stations),[])
+    for station in stations:
+        by_river[station.river].append(station.name)
+    return by_river
