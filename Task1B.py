2225a2585edd52dc57d567da2238ef5e09dbc331
jakedlyons <jakedenislyons@gmail.com>
2024-02-02 14:52:52 +0000

    Successful first test of stations_by_distance

--- Task1B.py
+++ Task1B.py
@@ -3,12 +3,13 @@ from floodsystem.stationdata import build_station_list
 
 
 def run():
-    # Put code here that demonstrates functionality
-    stations = build_station_list()
-    p = [52.2053, 0.1218]
+
+    stations = build_station_list() #Build station list
+    p = [52.2053, 0.1218] #Define coordinate to find distance between itself and each station
+
     ordered_stations = stations_by_distance(stations, p)
-    print('The closest 10 stations to city centre are :', ordered_stations[0:10])
-    print('The furthest 10 stations to city centre are :', ordered_stations[-10:])
+    print('\nThe closest 10 stations to city centre are :\n', ordered_stations[0:10])
+    print('\nThe furthest 10 stations to city centre are :\n', ordered_stations[-10:])
 
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IA Flood Warning System ***")
--- test_geo.py
+++ test_geo.py
@@ -1,5 +1,27 @@
-from floodsystem.utils import sorted_by_key  # noqa
-import haversine #Used in calculating shortest distance between two coordinates
+import floodsystem.geo
+
 
 """Unit tests for the geo module"""
 
+from floodsystem.geo import stations_by_distance
+from floodsystem.station import MonitoringStation
+import math
+import haversine
+
+def test_stations_by_distance(): #Test for function for generating a list of tuples (Station name , distance to coordinate p)
+    
+    # Create a station
+    s_id = "test-s-id"
+    m_id = "test-m-id"
+    label = "some station"
+    coord = (-2.0, 4.0)
+    trange = (-2.3, 3.4445)
+    river = "River X"
+    town = "My Town"
+    s = MonitoringStation(s_id, m_id, label, coord, trange, river, town)
+
+    stations = [s]
+    d_function = stations_by_distance(stations, p = [0., 0.]) #Find distance between our defined station and p through function
+    d_direct = haversine.haversine((-2., 4.), (0., 0.)) #Directly find distance between the two coords
+    
+    assert round(d_function[0][1], 8) == round(d_direct, 8) 
