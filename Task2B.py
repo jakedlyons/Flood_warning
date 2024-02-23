55488a27e178d65dee2fd02f627c7f58b463bf17
jakedlyons <jakedenislyons@gmail.com>
2024-02-23 17:42:01 +0000

    Creating Task 2 Files

---
+++ Task2B.py
@@ -0,0 +1,6 @@
+def run():
+    # Put code here that demonstrates functionality
+    print('goodbye world')
+
+if __name__ == "__main__":
+    run()
---
+++ Task2C.py
@@ -0,0 +1,6 @@
+def run():
+    # Put code here that demonstrates functionality
+    print('hello world')
+
+if __name__ == "__main__":
+    run()
