3f2cb5bcec1722ad39ab98ff6ee29c509176ac55
jakedlyons <jakedenislyons@gmail.com>
2024-02-25 17:40:48 +0000

    Finished Task 2E WITHOUT NECESARY DEFINED YET

--- Task2E.py
+++ Task2E.py
@@ -3,36 +3,27 @@ from floodsystem.plot import plot_water_levels
 from floodsystem.stationdata import build_station_list
 from floodsystem.datafetcher import fetch_measure_levels
 import datetime
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.stationdata import update_water_levels
 
 
 def run():
-    #Use Cam to test function
-        # Build list of stations
+    # Build list of stations
     stations = build_station_list()
 
-        # Station name to find
-    station_name = "Cam"
+    # Update latest level data for all stations
+    update_water_levels(stations)
 
-        # Find station
-    station_cam = None
-    for station in stations:
-        if station.name == station_name:
-            station_cam = station
-            break
-
-        # Check that station could be found. Return if not found.
-    if not station_cam:
-        print("Station {} could not be found".format(station_name))
-        return
+    #Make list of the 5 stations with the highest relative level
+    highest_stations = stations_highest_rel_level(stations, 5):
     
-
     dt = 10 #Number of days from now to be plotted
-    
-    #Create two lists of times with corresponding water levels
-    dates, levels = fetch_measure_levels(
-        station_cam.measure_id, dt=datetime.timedelta(days=dt))
+    for station in highest_stations:
+        #Create two lists of times with corresponding water levels
+        dates, levels = fetch_measure_levels(
+        station.measure_id, dt=datetime.timedelta(days=dt))
 
-    plot_water_levels(station_cam, dates, levels)
+        plot_water_levels(station, dates, levels)
     
 if __name__ == "__main__":
     print("*** Task 2E: CUED Part IA Flood Warning System ***\n")
