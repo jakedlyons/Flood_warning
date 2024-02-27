bf902a616e64f14a924f1590ed1f383a60a649fb
jakedlyons <jakedenislyons@gmail.com>
2024-02-27 09:08:25 +0000

    fix 2E for 5 stations NOT 4

--- Task2E.py
+++ Task2E.py
@@ -16,7 +16,7 @@ def run():
 
     #Make list of the 5 stations with the highest relative level
     highest_stations = []
-    for station in stations_highest_rel_level(stations, 5):
+    for station in stations_highest_rel_level(stations, 6):
         highest_stations.append(station[0])
     
     dt = 10 #Number of days from now to be plotted
